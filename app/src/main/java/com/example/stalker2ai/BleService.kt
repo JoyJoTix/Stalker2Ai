a3f72d10cdee0657401a3097498e11dc8e7cd662
Jo <JoyJotix@gmail.com>
2026-04-07 21:23:57 +0300

    Ver.3.16

--- app/src/main/java/com/example/stalker2ai/BleService.kt
+++ app/src/main/java/com/example/stalker2ai/BleService.kt
@@ -12,6 +12,7 @@ import android.bluetooth.BluetoothGattCallback
 import android.bluetooth.BluetoothGattCharacteristic
 import android.bluetooth.BluetoothGattDescriptor
 import android.bluetooth.BluetoothProfile
+import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.content.pm.ServiceInfo
